fd59248826eccb3ed4346d98ccc9e97bc6497a55
saif <s4saif.121@gmail.com>
2019-09-20 22:20:28 +0400

    added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require ('express'); 
 var app = express(); 
 app.use(express.static(__dirname + '/public'));
-app.locals.myVar = "GOOGLE";
+app.locals.myVar = "AZURE";
 app.get ("/", function (req,res) {
 	res.render ( "hello.ejs" );	
 	} );
